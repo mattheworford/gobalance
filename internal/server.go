8856d27ae3cb94e3d9d5388f428946abfd0e0e04
mattheworford <mattorford9@gmail.com>
2024-04-17 00:08:46 -0400

    Update server.go

--- internal/server.go
+++ internal/server.go
@@ -9,6 +9,14 @@ import (
 	"time"
 )
 
+type Server struct {
+	config      ServerConfig
+	isAlive     bool
+	statusMutex sync.RWMutex
+	proxy       *httputil.ReverseProxy
+	logger      *log.Logger
+}
+
 type ServerConfig struct {
 	URL       string
 	Timeout   time.Duration
@@ -16,12 +24,32 @@ type ServerConfig struct {
 	TLSConfig *tls.Config
 }
 
-type Server struct {
-	config      ServerConfig
-	isAlive     bool
-	statusMutex sync.RWMutex
-	proxy       *httputil.ReverseProxy
-	logger      *log.Logger
+func (s *Server) CheckHealth() bool {
+	s.statusMutex.RLock()
+	defer s.statusMutex.RUnlock()
+
+	if !s.isAlive {
+		return false
+	}
+
+	if !s.externalHealthCheck() {
+		s.logger.Println("External health check failed")
+		return false
+	}
+
+	return true
+}
+
+func (s *Server) externalHealthCheck() bool {
+	client := &http.Client{Timeout: s.config.Timeout}
+	resp, err := client.Get(s.config.URL)
+	if err != nil {
+		s.logger.Printf("Failed to connect to the server: %v", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK
 }
 
 func NewServer(config ServerConfig, logger *log.Logger) *Server {
@@ -36,9 +64,4 @@ func NewServer(config ServerConfig, logger *log.Logger) *Server {
 		},
 		logger: logger,
 	}
-}
-
-func (s *Server) CheckHealth() bool {
-	// Implement health check logic
-	return s.isAlive
 }
